723d5a48ec35d7c9e656090d291bca8ab8a9f244
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-05-20 08:23:55 +0545

    Basic Interface Program: 1

--- Interfaces and Generics/BasicInterfaces/Program.cs
+++ Interfaces and Generics/BasicInterfaces/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace BasicInterfaces
+{
+    interface IStorable
+    {
+        void Save();
+        void Load();
+        bool NeedsSave { get; set; }
+    }
+    class Document: IStorable
+    {
+        private string name;
+        public Document(string s)
+        {
+            name = s;
+            Console.WriteLine("Created a document with name: '{0}'",s);
+        }
+        public void Save() {
+            Console.WriteLine("Saving the document");
+        }
+        public void Load() {
+            Console.WriteLine("Loading the document");
+        }
+        public bool NeedsSave { get; set;}
+    }
+    class Program
+    {
+        static void Main(string[] args) {
+            Document d = new Document("Test Document");
+            d.Load();
+            d.Save();
+            d.NeedsSave = false;
+            Console.WriteLine("\nPress Enter to continue...");
+
+        }
+    }
+}
