854a3c7cbf7b0b1fc8858e6409d5c776aa4649a9
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-05-22 13:18:38 +0545

    Anonymous Delegates

---
+++ Delegates/AnonymousDelegates/AnonymousDelegates/AnonymousDelegates/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AnonymousDelegates
+{
+    class Program
+    {
+        public delegate string MyDelegate(int arg1,int arg2);   
+        static void Main(string[] args)
+        {
+            //TODO: Implement an anonymous delegate
+            MyDelegate f = delegate (int arg1, int arg2) {
+            return (arg1 + arg2).ToString();
+            };
+            Console.WriteLine("The result is "+ f(10,20));
+        }
+    }
+}
