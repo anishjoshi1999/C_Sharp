c652680add849fcae3737f7636d6da28c01a60e2
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-05-22 13:09:59 +0545

    Learning Delegates

---
+++ Delegates/BasicDelegates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BasicDelegates
+{
+    // TODO: declare the delegate type
+    public delegate string MyDelegate(int arg1,int arg2);   
+
+    class MyClass
+    {
+        // Delegates can be bound to instance members as well as
+        // static class functions
+        public string instanceMethod1(int arg1, int arg2)
+        {
+            return ((arg1 + arg2) * arg1).ToString();
+        }
+    }
+
+    class Program
+    {
+        // TODO: Create functions to serve as delegate implementations
+        static string func1(int a,int b)
+        {
+            return (a + b).ToString();
+        }
+        static string func2(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            // TODO: exercise each delegate function
+            MyDelegate f = func1;
+            Console.WriteLine("The number from func1 is: " + f(10,20));
+            f = func2;
+            Console.WriteLine("The number from func2 is: " + f(10,20));
+
+            // TODO: Use an instance function of a class as a delegate
+            MyClass mc = new MyClass();
+            f = mc.instanceMethod1;
+            Console.WriteLine("The number from instanceMethod is:" + f(10,20));
+
+        }
+    }
+}
---
+++ Delegates/ConsoleUI/ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿PrintDelegate PrintConsole = (string text) =>
+{
+    Console.WriteLine(text);
+};
+PrintDelegate PrintToFile = (string text) =>
+{
+    File.AppendAllText("./logs.txt", text);
+};
+static void ConnectToDatabase(PrintDelegate log)
+{
+    //Do the insertion
+    log("Inserting a new record into the database.");
+    //Insertion done
+    log("The record got inserted into the database.");
+}
+ConnectToDatabase(PrintConsole);
+
+
+delegate void PrintDelegate(string text);
